455c9d4ce8e490925617adf73cf1d87a7a2a645f
Moni7699 <mi4286803@gmail.com>
2026-06-29 12:15:11 +0530

    Clean up firebase-config.js by removing comments
    Removed comments in Bengali and cleaned up code.

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,7 @@
-// Firebase SDK মডিউল ইম্পোর্ট করা হচ্ছে (Free CDN)
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 import { getDatabase } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-database.js";
 
-// তোমার দেওয়া কনফিগারেশন
 const firebaseConfig = {
   apiKey: "AIzaSyApQOM_mtFZ16RiNJEaIUhb4iYFBIBRK58",
   authDomain: "mneet-spark.firebaseapp.com",
@@ -14,8 +12,6 @@ const firebaseConfig = {
   appId: "1:252201633700:web:1a1e7a2cff1f0b168ea331"
 };
 
-// Firebase ইনিশিয়ালাইজ করা
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getDatabase(app);
-
