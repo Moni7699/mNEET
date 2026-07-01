db966f9c76a660d30f90cad900fcdef3884b5542
Moni7699 <mi4286803@gmail.com>
2026-07-01 17:43:58 +0530

    Switch from Firestore to Realtime Database

--- firebase-config.js
+++ firebase-config.js
@@ -1,6 +1,5 @@
-// mNEET Global System Firebase Modular Integration Blueprint Config
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
-import { getFirestore, collection, addDoc, getDocs, doc, updateDoc, deleteDoc } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+import { getDatabase, ref, set, push, onValue, remove } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-database.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyApQOM_mtFZ16RiNJEaIUhb4iYFBIBRK58",
@@ -12,7 +11,9 @@ const firebaseConfig = {
   appId: "1:252201633700:web:1a1e7a2cff1f0b168ea331"
 };
 
-// Initializing engines
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
-export { collection, addDoc, getDocs, doc, updateDoc, deleteDoc };
+export const db = getDatabase(app);
+
+// Exporting Realtime Database core module operations
+export { ref, set, push, onValue, remove };
